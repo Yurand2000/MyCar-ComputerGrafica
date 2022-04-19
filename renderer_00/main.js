fb027f4ee2b780bd59fb8e2788dd977b0173dd30
Yuri Andriaccio <yurand2000@gmail.com>
2022-04-19 10:59:59 +0200

    Helper script to start http server, removed solution for renderer_00

--- renderer_00/main.js
+++ renderer_00/main.js
@@ -8,50 +8,18 @@ the FollowFromUpCamera always look at the car from a position abova right over t
 FollowFromUpCamera = function(){
 
   /* the only data it needs is the position of the camera */
-  this.frame = glMatrix.mat4.create();
+  this.pos = [0,0,0];
   
   /* update the camera with the current car position */
   this.update = function(car_position){
-    this.frame = car_position;
+    this.pos = car_position;
   }
 
   /* return the transformation matrix to transform from worlod coordiantes to the view reference frame */
   this.matrix = function(){
-    let eye = glMatrix.vec3.create();
-    let target = glMatrix.vec3.create();
-    let up = glMatrix.vec4.create();
-    
-    glMatrix.vec3.transformMat4(eye, [0  ,50,0], this.frame);
-    glMatrix.vec3.transformMat4(target, [0.0,0.0,0.0,1.0], this.frame);
-    glMatrix.vec4.transformMat4(up, [0.0,0.0,-1,0.0], this.frame);
-    
-    return glMatrix.mat4.lookAt(glMatrix.mat4.create(),eye,target,up.slice(0,3));	
+    return glMatrix.mat4.lookAt(glMatrix.mat4.create(),[ this.pos[0],50, this.pos[2]], this.pos,[0, 0, -1]);	
   }
 }
-
-/*
-the ChaseCamera always look at the car from behind the car, slightly above
-*/
-ChaseCamera = function(){
-
-  /* the only data it needs is the frame of the camera */
-  this.frame = [0,0,0];
-  
-  /* update the camera with the current car position */
-  this.update = function(car_frame){
-    this.frame = car_frame.slice();
-  }
-
-  /* return the transformation matrix to transform from worlod coordiantes to the view reference frame */
-  this.matrix = function(){
-    let eye = glMatrix.vec3.create();
-    let target = glMatrix.vec3.create();
-    glMatrix.vec3.transformMat4(eye, [0  ,1.5,4,1.0], this.frame);
-    glMatrix.vec3.transformMat4(target, [0.0,0.0,0.0,1.0], this.frame);
-    return glMatrix.mat4.lookAt(glMatrix.mat4.create(),eye, target,[0, 1, 0]);	
-  }
-}
-
 
 /* the main object to be implementd */
 var Renderer = new Object();
@@ -60,8 +28,6 @@ var Renderer = new Object();
 Renderer.cameras = [];
 // add a FollowFromUpCamera
 Renderer.cameras.push(new FollowFromUpCamera());
-Renderer.cameras.push(new ChaseCamera());
-
 // set the camera currently in use
 Renderer.currentCamera = 0;
 
@@ -132,13 +98,6 @@ Renderer.initializeObjects = function (gl) {
   Game.setScene(scene_0);
   this.car = Game.addCar("mycar");
   Renderer.triangle = new Triangle();
-
-  this.cube = new Cube(10);
-  this.createObjectBuffers(gl,this.cube);
-  
-  this.cylinder = new Cylinder(10);
-  this.createObjectBuffers(gl,this.cylinder );
-  
   Renderer.createObjectBuffers(gl, this.triangle);
 
   Renderer.createObjectBuffers(gl,Game.scene.trackObj);
@@ -147,87 +106,11 @@ Renderer.initializeObjects = function (gl) {
 	  	Renderer.createObjectBuffers(gl,Game.scene.buildingsObj[i]);
 };
 
-
-
 /*
 draw the car
 */
 Renderer.drawCar = function (gl) {
-
-    M                 = glMatrix.mat4.create();
-    rotate_transform  = glMatrix.mat4.create();
-    translate_matrix  = glMatrix.mat4.create();
-    scale_matrix      = glMatrix.mat4.create();
-  
-    glMatrix.mat4.fromTranslation(translate_matrix,[0,1,1]);
-    glMatrix.mat4.fromScaling(scale_matrix,[0.7,0.25,1]);
-    glMatrix.mat4.mul(M,scale_matrix,translate_matrix);
-    glMatrix.mat4.fromRotation(rotate_transform,-0.1,[1,0,0]);
-    glMatrix.mat4.mul(M,rotate_transform,M);
-    glMatrix.mat4.fromTranslation(translate_matrix,[0,0.1,-1]);
-    glMatrix.mat4.mul(M,translate_matrix,M);
-
-    Renderer.stack.push();
-    Renderer.stack.multiply(M);
-    gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, this.stack.matrix);
-
-    this.drawObject(gl,this.cube,[0.8,0.6,0.7,1.0],[0.8,0.6,0.7,1.0]);
-    Renderer.stack.pop();
-
-    Mw                 = glMatrix.mat4.create();
-    /* draw the wheels */
-    glMatrix.mat4.fromRotation(rotate_transform,3.14/2.0,[0,0,1]);
-    glMatrix.mat4.fromTranslation(translate_matrix,[1,0,0]);
-    glMatrix.mat4.mul(Mw,translate_matrix,rotate_transform);
-    
-    glMatrix.mat4.fromScaling(scale_matrix,[0.1,0.2,0.2]);
-    glMatrix.mat4.mul(Mw,scale_matrix,Mw);
-     /* now the diameter of the wheel is 2*0.2 = 0.4 and the wheel is centered in 0,0,0 */
-
-     
-    glMatrix.mat4.identity(M);
-    
-    glMatrix.mat4.fromTranslation(translate_matrix,[-0.8,0.2,-0.7]);
-    glMatrix.mat4.mul(M,translate_matrix,Mw);
-
-    Renderer.stack.push();
-    Renderer.stack.multiply(M);
-    gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, this.stack.matrix);
-  
-    this.drawObject(gl,this.cylinder,[1.0,0.6,0.5,1.0],[0.0,0.0,0.0,1.0]);
-    Renderer.stack.pop();
-
-    glMatrix.mat4.fromTranslation(translate_matrix,[0.8,0.2,-0.7]);
-    glMatrix.mat4.mul(M,translate_matrix,Mw);
-
-    Renderer.stack.push();
-    Renderer.stack.multiply(M);
-    gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, this.stack.matrix); 
-    this.drawObject(gl,this.cylinder,[1.0,0.6,0.5,1.0],[0.0,0.0,0.0,1.0]);
-    Renderer.stack.pop();
-
-    /* this will increase the size of the wheel to 0.4*1,5=0.6 */
-    glMatrix.mat4.fromScaling(scale_matrix,[1,1.5,1.5]);;
-    glMatrix.mat4.mul(Mw,scale_matrix,Mw);
-    
-    glMatrix.mat4.fromTranslation(translate_matrix,[0.8,0.25,0.7]);
-    glMatrix.mat4.mul(M,translate_matrix,Mw);
-  
-    Renderer.stack.push();
-    Renderer.stack.multiply(M);
-    gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, this.stack.matrix); 
-    Renderer.stack.pop();
-
-    this.drawObject(gl,this.cylinder,[1.0,0.6,0.5,1.0],[0.0,0.0,0.0,1.0]);
-
-    glMatrix.mat4.fromTranslation(translate_matrix,[-0.8,0.3,0.7]);
-    glMatrix.mat4.mul(M,translate_matrix,Mw);
-  
-    Renderer.stack.push();
-    Renderer.stack.multiply(M);
-    gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, this.stack.matrix); 
-    this.drawObject(gl,this.cylinder,[1.0,0.6,0.5,1.0],[0.0,0.0,0.0,1.0]);
-    Renderer.stack.pop();
+  this.drawObject(gl, this.triangle, [1, 1, 1, 1], [1, 1, 1, 1]);
 };
 
 
@@ -236,7 +119,7 @@ Renderer.drawScene = function (gl) {
   var width = this.canvas.width;
   var height = this.canvas.height
   var ratio = width / height;
-  this.stack = new MatrixStack();
+  var stack = new MatrixStack();
 
   gl.viewport(0, 0, width, height);
   
@@ -251,25 +134,23 @@ Renderer.drawScene = function (gl) {
   
   gl.uniformMatrix4fv(this.uniformShader.uProjectionMatrixLocation,     false,glMatrix.mat4.perspective(glMatrix.mat4.create(),3.14 / 4, ratio, 1, 500));
 
-  Renderer.cameras[Renderer.currentCamera].update(this.car.frame);
-
-
-  
+  Renderer.cameras[Renderer.currentCamera].update(this.car.position);
   var invV = Renderer.cameras[Renderer.currentCamera].matrix();
   
   // initialize the stack with the identity
-  this.stack.loadIdentity();
+  stack.loadIdentity();
   // multiply by the view matrix
-  this.stack.multiply(invV);
+  stack.multiply(invV);
 
   // drawing the car
-  this.stack.push();
-  this.stack.multiply(this.car.frame); // projection * viewport
-  //gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, stack.matrix);
-  this.drawCar(gl);
-  this.stack.pop();
+   stack.push();
 
-  gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, this.stack.matrix);
+  stack.multiply(this.car.frame);
+  gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, stack.matrix);
+  this.drawCar(gl);
+  stack.pop();
+
+  gl.uniformMatrix4fv(this.uniformShader.uModelViewMatrixLocation, false, stack.matrix);
 
   // drawing the static elements (ground, track and buldings)
 	this.drawObject(gl, Game.scene.groundObj, [0.3, 0.7, 0.2, 1.0], [0, 0, 0, 1.0]);
@@ -331,6 +212,4 @@ on_keydown = function(e){
 window.onload = Renderer.setupAndStart;
 
 
-update_camera = function (value){
-  Renderer.currentCamera = value;
-}
+
